275fd273baa6a5a2aec378c6c8e0ad9bfa2884b2
sarthak04agarwal <sarthak04agarwal@gmail.com>
2022-11-01 13:19:33 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,42 @@
+
+
+
+
+
+
+
+
+#def display_contact():
+
+#defsearch_contact():
+
+#def edit_contact():
+
+#def new_contact():
+
+#def remove_contact():
+
+
+
+
+
+menu = ("1. Display Contact Names \n2. Search for Contact \n3. \
+Edit Contact \n4. New Contact \n5. Remove Contact \n6. Exit" + "\n Please type your number: ")
+
+loop = True
+while(loop):
+    selection = input(menu)
+    if selection == "1":
+        #display_contact()
+    elif selection == "2":
+        #search_contact()
+    elif selection == "3":
+        #edit_contact()
+    elif selection == "4":
+        #new_contact()
+    elif selection == "5":
+        #remove_contact()
+    else:
+        loop = False
+
+

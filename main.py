6d59df2d5e3d51f58bfee8df134dfd2dd44cf2a8
sarthak04agarwal <sarthak04agarwal@gmail.com>
2022-11-16 13:22:45 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 
 contact_Names = ["Jon Doe", "Sarthak Agarwal", "Mary Poppins", "Tom Hanks"]
-contact_Number = ["(907) 376-0073", "(982) 964-3068", "(292) 350-9921", "(629) 246-5416"]
+contact_Number = ["907-376-0073", "982-964-3068", "292-350-9921", "629-246-5416"]
 contact_Email = ["jondoe@gmail.com", "s.agarwal1@share.epsb.ca", "marypoppins234@outlook.com", \
     "thanks@yahoo.com"]
 
 def display_contact():
+    print("ALL CONTACT NAMES")
     for x in contact_Names:
         print(x)
 
@@ -12,19 +13,17 @@ def search_contact():
     search_name = input("Please enter the name you want to search: ")
     for x in range(len(contact_Names)):
         if(contact_Names[x] == search_name):
-            print("Name: " + contact_Names[x] + "Contact Email: " + contact_Email[x] +\
-                 "Contact Phono number: " + contact_Number[x])
+            print("Name: " + contact_Names[x] + "\n" + "Contact Email: " + contact_Email[x] + "\n" +"Contact Phone number: " + contact_Number[x])
+        
 
 def edit_contact():
-    search_name = input("Please enter the name you want to search: ")
+    search_name = input("Please enter the name you want to edit: ")
     for x in range(len(contact_Names)):
         if(contact_Names[x] == search_name):
             edited_Name = input("Please enter the new name: ")
             edited_Phone = input("Please enter the new phone number: ")
             edited_Email = input("Please enter the new email address: ")
-            contact_Names[x] = edited_Name
-            contact_Email[x] = edited_Email
-            contact_Number[x] = edited_Phone
+    return edited_Email, edited_Phone, edited_Name
 
 def new_contact():
     add_Name = input("Please enter the new name: ")
@@ -37,8 +36,8 @@ def new_contact():
 
 def remove_contact():
     search_name = input("Please enter the name you want to search: ")
-    for x in range(len(contact_Names)):
-        if(contact_Names[x] == search_name):
+    #for x in range(len(contact_Names)):
+    #    if(contact_Names[x] == search_name):
             
 
 
@@ -57,7 +56,7 @@ while(loop):
     elif selection == "2":
         search_contact()
     elif selection == "3":
-        edit_contact()
+        contact_Names[x], contact_Number[x], contact_Email[x]= edit_contact()
     elif selection == "4":
         new_contact()
     elif selection == "5":
